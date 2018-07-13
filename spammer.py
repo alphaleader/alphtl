2123a304af40108eaa699d0ecde46e111994ca05
alpha_leader <38836329+hamidrt5nad@users.noreply.github.com>
2018-07-13 17:22:45 +0000

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,1 +1,14 @@
 
+#----------------------------------------------#
+
+#+          coded by alpha leader              +
+
+#+       telegram : @ALPHA_LEADER              +
+
+#----------------------------------------------#
+
+alpha = input('enter your text: ')
+
+print (alpha * 300)
+
+print ("@alpha_leader")
