5432e91ed5cd619168335813b9c3c6f812971e9a
alpha_leader <38836329+hamidrt5nad@users.noreply.github.com>
2018-07-13 17:37:02 +0000

    Create small-spam.py

---
+++ small-spam.py
@@ -0,0 +1,9 @@
+#-----------------------------------#
+#           coded by alpha leader    #
+#            telegram : @ALPHA_LEADER#
+#-------------------------------------#
+alpha = input('enter your text: ')
+
+print (alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" + alpha + "\n" )
+
+print ("@alpha_leader")
